667c011b27a3f75d4f6b2f6f4b1db1d3af8f2096
gitdagray <DaveOnEleven@gmail.com>
2021-04-17 08:20:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# "How to Build a Calculator with Vanilla Javascript"
+
+✅ [Check out my YouTube Channel with all of my tutorials](https://www.youtube.com/DaveGrayTeachesCode).
+
+**Description:**
+
+Learn how to build a calculator with Vanilla Javascript in this JS beginners project tutorial. We'll use the Windows 10 calculator as a guide and build a calculator clone with Vanilla Javascript that has the same functionality.
+
+[YouTube Tutorial](https://youtu.be/XyVIi6BipR8) for this repository.
+
+My [8 hour JavaScript course tutorial video](https://youtu.be/EfAl9bwzVZk) will help if you are just getting started with Javascript.
+
+### Academic Honesty
+
+**DO NOT COPY FOR AN ASSIGNMENT** - Avoid plagiargism and adhere to the spirit of this [Academic Honesty Policy](https://www.freecodecamp.org/news/academic-honesty-policy/).
---
+++ js/main.js
@@ -0,0 +1,152 @@
+const initApp = () => {
+
+    const currentValueElem = document.querySelector('.currentvalue');
+    const previousValueElem = document.querySelector('.previousvalue');
+    let itemArray = [];
+    const equationArray = [];
+    let newNumberFlag = false;
+
+    const inputButtons = document.querySelectorAll('.number');
+    inputButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+
+            const newInput = event.target.textContent;
+            if (newNumberFlag) {
+                currentValueElem.value = newInput;
+                newNumberFlag = false;
+            } else {
+                currentValueElem.value =
+                    currentValueElem.value == 0
+                        ? newInput
+                        : `${currentValueElem.value}${newInput}`;
+            }
+        });
+    });
+
+    const opButtons = document.querySelectorAll('.operator');
+    opButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+
+            // equal sign showing
+            if (newNumberFlag) {
+                previousValueElem.textContent = "";
+                itemArray = [];
+            }
+
+            const newOperator = event.target.textContent;
+            const currentVal = currentValueElem.value;
+
+            // need number first 
+            if (!itemArray.length && currentVal == 0) return;
+
+            // begin new equation 
+            if (!itemArray.length) {
+                itemArray.push(currentVal, newOperator);
+                previousValueElem.textContent =
+                    `${currentVal} 
+                     ${newOperator}`;
+                return newNumberFlag = true;
+            }
+
+            // complete equation 
+            if (itemArray.length) {
+
+                itemArray.push(currentVal); // 3rd elem
+
+                const equationObj = {
+                    num1: parseFloat(itemArray[0]),
+                    num2: parseFloat(currentVal),
+                    op: itemArray[1]
+                }
+
+                equationArray.push(equationObj);
+                const equationString =
+                    `${equationObj['num1']} 
+                     ${equationObj['op']} 
+                     ${equationObj['num2']}`;
+
+                const newValue = calculate(equationString, currentValueElem);
+
+                previousValueElem.textContent =
+                    `${newValue} ${newOperator}`;
+
+                // start new equation 
+                itemArray = [newValue, newOperator];
+                newNumberFlag = true;
+                console.log(equationArray);
+            }
+        });
+    });
+
+    const equalsButton = document.querySelector('.equals');
+    equalsButton.addEventListener('click', () => {
+        const currentVal = currentValueElem.value;
+        let equationObj;
+
+        // pressing equals repeatedly
+        if (!itemArray.length && equationArray.length) {
+            const lastEquation = equationArray[equationArray.length - 1];
+            equationObj = {
+                num1: parseFloat(currentVal),
+                num2: lastEquation.num2,
+                op: lastEquation.op
+            }
+        } else if (!itemArray.length) {
+            return currentVal;
+        } else {
+            itemArray.push(currentVal);
+            equationObj = {
+                num1: parseFloat(itemArray[0]),
+                num2: parseFloat(currentVal),
+                op: itemArray[1]
+            }
+        }
+
+        equationArray.push(equationObj);
+
+        const equationString =
+            `${equationObj['num1']} ${equationObj['op']} ${equationObj['num2']}`;
+
+        calculate(equationString, currentValueElem);
+
+        previousValueElem.textContent = `${equationString} =`;
+
+        newNumberFlag = true;
+        itemArray = [];
+        console.log(equationArray);
+    });
+
+    const clearButtons = document.querySelectorAll('.clear, .clearEntry');
+    clearButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            currentValueElem.value = 0;
+            if (event.target.classList.contains('clear')) {
+                previousValueElem.textContent = '';
+                itemArray = [];
+                equationArray = [];
+            }
+        });
+    });
+
+    const deleteButton = document.querySelector('.delete');
+    deleteButton.addEventListener('click', () => {
+        currentValueElem.value = currentValueElem.value.slice(0, -1);
+    });
+
+    const signChangeButton = document.querySelector('.signChange');
+    signChangeButton.addEventListener('click', () => {
+        currentValueElem.value = parseFloat(currentValueElem.value) * -1;
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+
+const calculate = (equation, currentValueElem) => {
+    const regex = /(^[*/=])|(\s)/g;
+    equation.replace(regex, '');
+    const divByZero = /(\/0)/.test(equation);
+    if (divByZero) return currentValueElem.value = 0;
+    return currentValueElem.value = eval(equation);
+}
